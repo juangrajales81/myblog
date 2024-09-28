c17e6f22c73174e8cf919cc50508b62b212bfd23
Juan Guillermo Grajales <juangrajales81@gmail.com>
2024-09-28 09:33:13 -0500

    Added Post Model

--- blog/models.py
+++ blog/models.py
@@ -9,11 +9,11 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
-    # author = models.ForeignKey(
-    #     settings.,
-    #     on_delete=models.CASCADE,
-    #     related_name='blog_posts'
-    # )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
